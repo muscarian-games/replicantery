9d8681c134ed08fd5ce63f10bf06cf61ca2b715b
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2020-05-13 22:37:28 -0400

    Accusing a replicant kills them if npc

--- bundles/tiny-areas/behaviors/npc/replicant.js
+++ bundles/tiny-areas/behaviors/npc/replicant.js
@@ -2,9 +2,14 @@
 
 const { Logger } = require('ranvier');
 const { generateCodename } = require('../../../../lib/codenames');
+const { handleNpcDeath } = require('../../../../lib/death');
 
 module.exports = {
   listeners: {
+    death: state => function (config) {
+      handleNpcDeath(state, this);
+    },
+
     spawn: state => function (config) {
       if (!this.metadata) {
         this.metadata = {};
--- bundles/tiny-commands/commands/accuse.js
+++ bundles/tiny-commands/commands/accuse.js
@@ -6,11 +6,16 @@ const getOtherCharactersInRoom = require('../lib/getOtherCharactersInRoom');
 
 module.exports = {
   command: (state) => (args, player) => {
+    if (player.metadata.role !== 'detective') {
+      return B.sayAt(player, 'Who are you to accuse anyone?');
+    }
+
     // If no args, ask for a target.
     if (!args) {
       return B.sayAt(player, 'Who are you accusing?');
     }
 
+    // Try to find target of accusation based on args.
     const { room } = player;
 
     const characters = getOtherCharactersInRoom(room, player);
@@ -25,21 +30,28 @@ module.exports = {
     }
 
     const accusedCodename = characterAccused.metadata.name;
+    const playerCodename = player.metadata.name;
     B.sayAt(player, `You accuse ${accusedCodename} of being a replicant!`);
 
+    // Handle the consequences of the accusation.
     if (characterAccused.metadata.role === 'replicant') {
       B.sayAt(player, `They must be a replicant! Your scanner melts them down to a puddle of goo.`);
+      if (!characterAccused.isNpc) {
+        B.sayAt(characterAccused, `${playerCodename} fires their scanner at you! You're doomed!`);
+      }
+      B.sayAtExcept(player.room, `${playerCodename} fires their scanner at ${accusedCodename}. ${accusedCodename} melts into a gooey pile of machinery!`, [player, characterAccused]);
       player.emit('point');
       characterAccused.emit('death');
     } else {
       // TODO: Player cannot accuse for 30s
       B.sayAt(player, `Having wrongly accused ${accusedCodename}, your replicant scanner malfunctions!`);
       characterAccused.emit('point');
-      player.emit('point', -1)
-      // player.emit('death')
+      player.emit('point', -1);
+      if (!characterAccused.isNpc) {
+        B.sayAt(characterAccused, `${playerCodename} fires their scanner at you! It starts to malfunction.`);
+      }
+      B.sayAtExcept(player.room, `${playerCodename} fires their scanner at ${accusedCodename}. The scanner starts emitting sparks as it refuses to melt an organic human!`, [player, characterAccused]);
+      characterAccused.emit('accused', player);
     }
-
-    // Player factions: Replicant Hunter vs. Replicant Helper
-    // NPC factions: Human Citizen, Replicant Spy
   }
 };
